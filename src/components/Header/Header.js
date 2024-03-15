571a0ba5e231f68ad3da6dd40646da595c8a04b7
SteveR <steve17rouse@gmail.com>
2024-03-14 20:55:26 -0600

    initial comit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation'; // Adjust the path if necessary
+import './Header.css'; // Assume you also have a CSS file for styling
+
+function Header() {
+  return (
+    <header>
+      <h1>Steve Rouse</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
