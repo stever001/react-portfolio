6488797e63c7c656aea10b42a9cfd4837dff0cc0
SteveR <steve17rouse@gmail.com>
2024-03-21 00:47:38 -0600

    updated styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css'; // If you have a corresponding CSS file for Home
 function Home() {
   return (
     <div className="home-container">
-      <h1>Welcome to my portfolio!</h1>
+      <h2>Welcome to my portfolio!</h2>
       {/* You can add more content here */}
     </div>
   );
