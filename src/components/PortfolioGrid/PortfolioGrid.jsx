21c0566027d21845965f7c4911e653b567606072
SteveR <steve17rouse@gmail.com>
2024-03-28 00:06:38 -0600

    updated styling

--- src/components/PortfolioGrid/PortfolioGrid.jsx
+++ src/components/PortfolioGrid/PortfolioGrid.jsx
@@ -1,7 +1,8 @@
-// /components/PortfolioGrid/PortfolioGrid.jsx
 import React from 'react';
 import './PortfolioGrid.css';
 import portfolioItems from '../../data/portfolioItems.json'; // Adjust the path as necessary
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const PortfolioGrid = () => {
   return (
@@ -11,7 +12,13 @@ const PortfolioGrid = () => {
           <img src={item.imageUrl} alt={item.title} className="portfolio-image" />
           <div className="portfolio-item-info">
             <h3>{item.title}</h3>
-            <p>{item.description}</p>
+            <p>
+              {item.description}
+              {/* Assuming you add 'githubUrl' in your JSON data */}
+              <a href={item.githubUrl} target="_blank" rel="noopener noreferrer" className="github-icon">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </p>
           </div>
         </div>
       ))}
@@ -20,4 +27,3 @@ const PortfolioGrid = () => {
 };
 
 export default PortfolioGrid;
-
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
-import './Resume.css'; // If you have a corresponding CSS file for Resume
+import './Resume.css';
 
 function Resume() {
-  return <div>Resume content will go here.</div>;
+  return (
+    <div className="resume-container">
+      <h1>My Web Development Skills</h1>
+      <div className="skills-section">
+        <div className="skills-column">
+          <h2>Front End</h2>
+          <ul>
+            <li>HTML5 & CSS3</li>
+            <li>Bootstrap</li>
+            <li>JavaScript (ES6+)</li>
+            <li>JQuery</li>
+            <li>React</li>
+            <li>Responsive Design</li>
+            <li>Webpack & Babel</li>
+          </ul>
+        </div>
+        <div className="skills-column">
+          <h2>Back End</h2>
+          <ul>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>RESTful API Design</li>
+            <li>GraphQL</li>
+            <li>MongoDB/Mongoose</li>
+            <li>SQL (PostgreSQL & MySQL)</li>
+            <li>Authentication & Authorization (JWT)</li>
+          </ul>
+        </div>
+      </div>
+      {/* Replace the button with an <a> tag for downloading the resume */}
+      <a href="/resume/rouse23.docx" download="rouse23.docx" className="download-btn">
+        Download Resume
+      </a>
+    </div>
+  );
 }
 
 export default Resume;
