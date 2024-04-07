821e8256fd18e5996e1efac130354de506a4efd7
SteveR <steve17rouse@gmail.com>
2024-04-07 00:48:48 -0600

    updated to download newest resume.

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -32,7 +32,7 @@ function Resume() {
         </div>
       </div>
       {/* Replace the button with an <a> tag for downloading the resume */}
-      <a href="/resume/rouse24.docx" download="rouse23.docx" className="download-btn">
+      <a href="/resume/rouse24.pages" download="rouse24.pages" className="download-btn">
         Download Resume
       </a>
     </div>
